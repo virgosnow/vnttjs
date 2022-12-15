34af5d0b6800aa35ed5e6a74362d722a51aecf3b
virgosnow <49341207+virgosnow@users.noreply.github.com>
2022-12-15 19:12:09 +0800

    修正一个判断问题

--- VNTT翻译辅助.user.js
+++ VNTT翻译辅助.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VNTT翻译辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.54
+// @version      0.55
 // @description  为VNTT翻译平台集合机器翻译/术语提示/翻译记忆等常用CAT功能
 // @author       元宵
 // @match        https://a.vntt.app/project*
@@ -396,7 +396,7 @@ async function find_duplicate(jpText, submit, editArea){
         url:searchUrl,
     }
     const res = await Request(options);
-    console.log(res.responseText.split('<div class="original">').length)
+    const length = res.responseText.split('<div class="original">').length
     if (length < 3 || editArea.value !== "") {
         return
     }
