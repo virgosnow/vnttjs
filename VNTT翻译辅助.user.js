6f87bbee64c20950a1d4db2f4c7c8d6427c440fc
shigui <virgosnow@foxmail.com>
2023-03-16 18:32:21 +0800

    升一个小版本

--- VNTT翻译辅助.user.js
+++ VNTT翻译辅助.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VNTT翻译辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.60
+// @version      0.61
 // @description  为VNTT翻译平台集合机器翻译/术语提示/翻译记忆等常用CAT功能
 // @author       元宵
 // @match        https://a.vntt.app/project*
