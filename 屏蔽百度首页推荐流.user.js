c860ea131f83536d7b47094f7b156dd834582fa0
virgosnow <49341207+virgosnow@users.noreply.github.com>
2023-04-21 09:58:52 +0800

    Add files via upload

---
+++ 屏蔽百度首页推荐流.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         屏蔽百度首页推荐流
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  屏蔽百度个人首页新出的推荐流
+// @author       Virgosnow
+// @match        https://www.baidu.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 屏蔽推荐两个字
+    for (let e of document.getElementsByClassName("s-menu-item")) {
+        if (e.getAttribute("data-id") != 100) {
+            e.remove()
+        }
+    }
+    // 屏蔽推荐流载体
+    for (let e of document.getElementsByClassName("s-content")) {
+        if (e.getAttribute("data-id") != 100) {
+            e.remove()
+        }
+    }
+    // 延时200毫秒，等待加载完毕时点击我的关注
+    setTimeout(function(){document.getElementById("s_menu_mine").click()}, 200)
+})();
